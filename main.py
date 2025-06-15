f461c48e3da6630eb8af98261c092bc264f69f3b
Phou Mintphichun <phichun2022@gmail.com>
2025-06-15 09:24:43 +0700

    main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from pygame import *
+
+''' colors '''
+background = (200, 255, 255)
+
+
+
+window = 
+
+
+run = True
+
+while run:
+
+    for e in event.get():
+        if e.type == QUIT():
+            run = False
