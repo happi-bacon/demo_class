f1000d89ec34b9d993da1fc9bd3ba297079839e3
Phou Mintphichun <phichun2022@gmail.com>
2025-06-26 15:14:13 +0700

    main.py

--- main.py
+++ main.py
@@ -1,19 +1,18 @@
 from pygame import *
 from spriteClass import Player
 
-''' colors '''
+# Colors
 background = (200, 255, 255)
 
-'''var'''
+# Dimensions
 width = 600
 height = 500
 
-
-window = display.set_mode(width, height)
-window.fill(background)
-
+# Window setup
+window = display.set_mode((width, height))
 clock = time.Clock()
 
+# Players and ball
 pad1 = Player(player_image='images/pad.jpg',
               player_x=20, player_y=220, 
               player_speed=4, width=50, height=150)
@@ -26,42 +25,39 @@ ball = Player(player_image='images/ball.jpg',
               player_x=200, player_y=200, 
               player_speed=4, width=65, height=65)
 
-
-
+# Game state
 run = True
-finish = True
-ball_speed_x = 1
-ball_speed_y = 1
+ball_speed_x = 3
+ball_speed_y = 3
 
 while run:
-
-    if finish == True:
-        window.fill(background)
-
-        pad1.reset(window=window)
-        pad1.update_left()
-
-        pad2.reset(window=window)
-        pad2.update_right()
-
-        ball.reset(window=window)
-
-        ball.rect.x += ball_speed_x
-        ball.rect.y += ball_speed_y
-
-        if ball.rect.y > 500-65 or ball.rect.y < 0:
-            ball_speed_y *= 1
-
-        if sprite.collide.rect(pad1, ball) or sprite.collide.rect.(pad2, ball):
-            ball_speed_x *= -1
-
-
-    if ball.rect.y > 65
-
     for e in event.get():
-        if e.type == QUIT():
+        if e.type == QUIT:
             run = False
 
-            
+    window.fill(background)
+
+    # Move and draw paddles
+    pad1.update_left()
+    pad1.reset(window)
+
+    pad2.update_right()
+    pad2.reset(window)
+
+    # Move ball
+    ball.rect.x += ball_speed_x
+    ball.rect.y += ball_speed_y
+
+    # Bounce off top and bottom
+    if ball.rect.top <= 0 or ball.rect.bottom >= height:
+        ball_speed_y *= -1
+
+    # Bounce off paddles
+    if pad1.rect.colliderect(ball.rect) or pad2.rect.colliderect(ball.rect):
+        ball_speed_x *= -1
+
+    # Draw ball
+    ball.reset(window)
+
     display.update()
     clock.tick(60)
